5ad132a946cbb95dabad0cb69acb2ad8510b2139
Georgiy <goshazvir@gmail.com>
2014-01-30 08:20:59 +0200

    init

--- js/common.js
+++ js/common.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
+$(document).ready(function () {
+	var date1 = new Date(2014, 0, 1);
+	var date2 = new Date(2014, 0, 8);
+	var date3 = new Date(2014, 4, 1);
+	var date4 = new Date(2014, 4, 5);
+    $('.datepicker').datepicker({
+    	numberOfMonths: [3, 2],
+        beforeShowDay: function (date) {
+            debugger
+            if (date >= date1 && date <= date2) {
+                return [true, 'ui-state-unavailable', ''];
+            }
+            return [true, '', ''];
+        }
+    });
 
+    // function element exists
+    jQuery.fn.exists = function() {
+      return $(this).length;
+    }
+    
+    // scrollable
+    if ($('.building__gallery-wrap').exists()){
+        $('.building__gallery-wrap .caro').scrollable({
+            next:'.slide__arrow-next',
+            prev:'.slide__arrow-prev',
+            items: '.slides'
+        });
+    };
+
+    $(".building__gallery-wrap .slides a").click(function() {
+        $(".building__media img").attr('src', $(this).attr('href'));
+        $(this).parent().addClass('active').siblings().removeClass('active');
+        return false;
+    });
 });
---
+++ js/jquery.ui.datepicker-da.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+	(function($) {
+	    $.ui.datepicker.regional['da'] = {
+	                renderer: $.ui.datepicker.defaultRenderer,
+	        monthNames: ['Januar','Februar','Marts','April','Maj','Juni',
+	        'Juli','August','September','Oktober','November','December'],
+	        monthNamesShort: ['Jan','Feb','Mar','Apr','Maj','Jun',
+	        'Jul','Aug','Sep','Okt','Nov','Dec'],
+	                dayNames: ['Søndag','Mandag','Tirsdag','Onsdag','Torsdag','Fredag','Lørdag'],
+	                dayNamesShort: ['Søn','Man','Tir','Ons','Tor','Fre','Lør'],
+	                dayNamesMin: ['Sø','Ma','Ti','On','To','Fr','Lø'],
+	        dateFormat: 'dd-mm-yyyy',
+	                firstDay: 1,
+	                prevText: '&#x3c;Forrige', prevStatus: '',
+	                prevJumpText: '&#x3c;&#x3c;', prevJumpStatus: '',
+	                nextText: 'Næste&#x3e;', nextStatus: '',
+	                nextJumpText: '&#x3e;&#x3e;', nextJumpStatus: '',
+	                currentText: 'Idag', currentStatus: '',
+	                todayText: 'Idag', todayStatus: '',
+	                clearText: '-', clearStatus: '',
+	                closeText: 'Luk', closeStatus: '',
+	                yearStatus: '', monthStatus: '',
+	                weekText: 'Uge', weekStatus: '',
+	                dayStatus: 'DD d MM',
+	                defaultStatus: '',
+	                isRTL: false
+	        };
+	    $.extend($.ui.datepicker.defaults, $.ui.datepicker.regional['da']);
+	})(jQuery);
+
+});

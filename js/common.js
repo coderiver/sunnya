47b7d46c282dc5d1e383c0c5570099ef283b3d88
Georgiy <goshazvir@gmail.com>
2014-01-30 10:53:24 +0200

    push

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
 	var date1 = new Date(2014, 0, 1);
 	var date2 = new Date(2014, 0, 8);
-	var date3 = new Date(2014, 4, 1);
-	var date4 = new Date(2014, 4, 5);
+	var date3 = new Date(2014, 9, 9);
+	var date4 = new Date(2014, 4, 21);
     $('.datepicker').datepicker({
     	numberOfMonths: [3, 2],
         beforeShowDay: function (date) {
@@ -10,27 +10,13 @@ $(document).ready(function () {
             if (date >= date1 && date <= date2) {
                 return [true, 'ui-state-unavailable', ''];
             }
+            if (date >= date3 && date <= date4) {
+              return [true, 'blabla', 'tooltipText'];
+            }
             return [true, '', ''];
         }
     });
 
     // function element exists
-    jQuery.fn.exists = function() {
-      return $(this).length;
-    }
-    
-    // scrollable
-    if ($('.building__gallery-wrap').exists()){
-        $('.building__gallery-wrap .caro').scrollable({
-            next:'.slide__arrow-next',
-            prev:'.slide__arrow-prev',
-            items: '.slides'
-        });
-    };
 
-    $(".building__gallery-wrap .slides a").click(function() {
-        $(".building__media img").attr('src', $(this).attr('href'));
-        $(this).parent().addClass('active').siblings().removeClass('active');
-        return false;
-    });
 });
